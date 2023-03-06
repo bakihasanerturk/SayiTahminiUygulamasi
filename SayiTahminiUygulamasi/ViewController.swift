59b000abfbeb378af7933b676e2cc9159d41629a
bakihasanerturk <hasanbakierturk@gmail.com>
2023-03-06 18:01:20 +0300

    Initial Commit

---
+++ SayiTahminiUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SayiTahminiUygulamasi
+//
+//  Created by Baki Hasan Ertürk on 6.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
